2d6cb632fed6a4931cb98e1ece6f6057521ed4e8
RehanRaza111 <Rehan.jafferi110@gmail.com>
2023-06-24 16:12:35 +0500

    added new project

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+
+import MovieCard from './MovieCard';
+
+import Footer from './Footer';
+import './App.css';
+
+import SearchIcon from './search.svg';
+
+// 7b71ee54
+
+// const API_URL = 'http://www.omdbapi.com?apikey=7b71ee54';
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=7b71ee54';
+
+// const movie1 = {
+//     "Title": "Guardians of the Galaxy Vol. 2",
+//     "Year": "2017",
+//     "imdID": "tt3896198",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
+
+
+// };
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        
+        const response = await fetch(`${API_URL}&s=${title}`); 
+        const data = await response.json();
+        setMovies(data.Search);
+        // console.log(data.Search)
+    }   
+
+        
+    useEffect(() => {
+        searchMovies('Spiderman');
+
+    }, []);
+    
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input type="text" placeholder='Serach for Movies' value={searchTerm}
+                    onChange={(e) => 
+                    setSearchTerm(e.target.value)
+                    }
+                />
+                <img src={SearchIcon}
+                    alt="search" 
+                    onClick={() => 
+                    searchMovies(searchTerm)}
+                />
+
+            </div>
+
+
+            {
+                movies?.length > 0 
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={ movie} />
+                            ))}
+            </div>
+                    ) :
+                    (
+                        <div className="empty">
+                            <h2>No Movies found</h2>
+                     </div>
+                    )
+            }
+
+
+            
+            <Footer/>
+        </div>
+    );
+    
+}
+
+
+export default App;  
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    
+    return (
+        <div className="footer">
+            <p>&copy; 2023 by Developer Rehan Raza</p>
+        </div>
+    )
+
+}
+
+
+export default Footer;
